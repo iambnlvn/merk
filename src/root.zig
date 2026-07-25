42ab052f7ecdc1a7aed4152211f1e8234a5066a2
iambnlvn <iambnlvn@gmail.com>
2026-07-25 23:59:21 +0100

    chore: add missing io import to root mod

--- src/root.zig
+++ src/root.zig
@@ -3,6 +3,7 @@ const std = @import("std");
 pub const crypto = @import("crypto/crypto.zig");
 pub const compression = @import("compression/compression.zig");
 pub const merkle = @import("merkle/merkle.zig");
+pub const io = @import("storage/io.zig");
 
 test {
     std.testing.refAllDeclsRecursive(@This());
