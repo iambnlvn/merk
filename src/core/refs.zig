711ee16d2489805f64e90e01d44543374d831af0
iambnlvn <iambnlvn@gmail.com>
2026-08-05 22:20:49 +0100

    refactor(wip): introduce refs.zig for exposing /refs module

---
+++ src/core/refs.zig
@@ -0,0 +1,3 @@
+pub const ReferenceStore = @import("./refs/refs.zig").ReferenceStore;
+pub const ChannelName = @import("./refs/refs.zig").ChannelName;
+pub const Current = @import("./refs/refs.zig").Current;
