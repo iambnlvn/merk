f0722b01a7aaa6a83c967154796c4275e5612a0e
iambnlvn <iambnlvn@gmail.com>
2026-08-07 00:43:54 +0100

    chore: cleanup old exports

--- src/storage/storage.zig
+++ src/storage/storage.zig
@@ -3,11 +3,5 @@ const os_fs_mod = @import("os_fs.zig");
 const mem_fs_mod = @import("mem_fs.zig");
 
 pub const Vfs = vfs.Vfs;
-pub const FileSystem = Vfs;
 pub const OsFs = os_fs_mod.OsFs;
 pub const MemoryFs = mem_fs_mod.MemoryFs;
-pub const RealFs = OsFs;
-pub const TestFs = MemoryFs;
-
-pub const os_fs = OsFs;
-pub const mem_fs = MemoryFs;
